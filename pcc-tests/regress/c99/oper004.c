57122dfc6606cf574175ff422b04cc67810551ff
plunky <>
2011-11-07 14:27:04 +0000

    correct the comment and add some further testing for comma operations sequence in the end, return 0 for successful test

--- pcc-tests/regress/c99/oper004.c
+++ pcc-tests/regress/c99/oper004.c
@@ -9,7 +9,16 @@ int main(int *argc, char **argv)
 		 b = 1,
 		 c = 1;  
 
-	foo(a, (b=5, b++), c);	// (b=5,b++) - one argument with val=6
+	foo(a, (b=5, b++), c);	// (b=5,b++) - one argument with val=5
 
-	return 1; 
+	if ((b = 5, b + 3) != 8)
+		return 1;
+
+	if ((b = 5, b++) != 5)
+		return 1;
+
+	if (b != 6)
+		return 1;
+
+	return 0; 
 }
