ac1f02329a0ffb8388dd502365cd0c2c0990e229
plunky <>
2011-11-08 10:54:10 +0000

    explanatory comment, some whitespace

--- pcc-tests/regress/c99/arith999.c
+++ pcc-tests/regress/c99/arith999.c
@@ -1,9 +1,15 @@
-/* must fail; from c99 */
-int main()
-{
-	int i=1, a[10]; 
+/*
+ * C99 section 6.5 note 2
+ *
+ * multiple modifications between sequence points is undefined behaviour,
+ * meaning both these statements should fail
+ */
 
-	i = ++i + 1 ;
+int main(int argc, char *argv[])
+{
+	int i=1, a[10];
+
+	i = ++i + 1;
 	a[i++] = i;
 
 	return 0;
