e294e0a3347522805794cfe4d36dce3a9dc76ccd
ragge <>
2012-09-22 09:59:43 +0000

    I386 only.

--- pcc-tests/regress/jira/PCC-158.c
+++ pcc-tests/regress/jira/PCC-158.c
@@ -8,7 +8,9 @@ struct _TEB;
 static inline __attribute__((always_inline)) struct _TEB * __attribute__((__stdcall__)) NtCurrentTeb(void)
 {
 	    struct _TEB *teb;
+#ifdef __i386__
 		     __asm__(".byte 0x64\n\tmovl (0x18),%0" : "=r" (teb));
+#endif
 			      return teb;
 }
 
