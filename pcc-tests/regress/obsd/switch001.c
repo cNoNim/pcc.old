0b910ef2d085224aecd562af2401c9405798c28c
ragge <>
2012-09-22 10:21:51 +0000

    Only 32-bit.

--- pcc-tests/regress/obsd/switch001.c
+++ pcc-tests/regress/obsd/switch001.c
@@ -7,12 +7,13 @@
 int
 main(int argc, char *argv[])
 {
+#ifdef __i386__
 	unsigned long long i = (unsigned long)~0 + (unsigned long long)2;
 
 	switch (i) {
 	case 1:
 		return 1;
 	}
-
+#endif
 	return 0;
 }
