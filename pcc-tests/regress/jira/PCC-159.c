f9e46647bfe672750ff31fde77588ac214af8974
plunky <>
2014-06-06 11:55:25 +0000

    a few further test cases from JIRA

--- pcc-tests/regress/jira/PCC-159.c
+++ pcc-tests/regress/jira/PCC-159.c
@@ -4,11 +4,17 @@
  *
  * pcc -c PCC-159.c
  */
-int (__attribute__((__stdcall__)) bar)(int b);
+
+int __attribute__((__stdcall__)) bar(int);
+
+int bar(int a)
+{
+	return a;
+}
 
 int foo(int a)
 {
 	    return bar(a);
 }
 
-
+int main(int argc, char *argv[]) { return 0; }
